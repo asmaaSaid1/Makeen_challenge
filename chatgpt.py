27bd776324598d0a3aac316e31ebe383726314fe
asmaaSaid1 <130406417+asmaaSaid1@users.noreply.github.com>
2023-05-29 07:39:28 +0400

    Add files via upload

---
+++ chatgpt.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 22:13:18 2023
+
+@author: Asma
+"""
+
+
+
+
+
+
+import openai
+openai.api_key = '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'
+messages = [ {"role": "system", "content":
+			"You are a intelligent assistant."} ]
+while True:
+	message = input("User : ")
+	if message:
+		messages.append(
+			{"role": "user", "content": message},
+		)
+		chat = openai.ChatCompletion.create(
+			model="gpt-3.5-turbo", messages=messages
+		)
+	reply = chat.choices[0].message.content
+	print(f"ChatGPT: {reply}")
+	messages.append({"role": "assistant", "content": reply})
+
+
+
+
